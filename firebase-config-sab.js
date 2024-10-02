71041957e03a6321c49e37f9abf380b639392c6f
Angeldfire2 <166663144+Angeldfire-2@users.noreply.github.com>
2024-10-02 15:39:24 -0600

    Update firebase-config-sab.js

--- firebase-config-sab.js
+++ firebase-config-sab.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY_SAB,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN_SAB,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID_SAB,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET_SAB,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID_SAB,
-    appId: process.env.NEXT_PUBLIC_APP_ID_SAB
+    apiKey: process.env.REACT_APP_API_KEY_SAB,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN_SAB,
+    projectId: process.env.REACT_APP_PROJECT_ID_SAB,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET_SAB,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_SAB,
+    appId: process.env.REACT_APP_APP_ID_SAB
 };
 
 firebase.initializeApp(firebaseConfig);
